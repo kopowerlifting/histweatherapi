7a5c533bd4945fba72875641df0ed216ef17ce79
kizkiz <yetislayer7@hotmail.com>
2024-08-10 13:55:06 +1000

    Show station list on homepage

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data/stations.txt", skiprows=17)
+stations = stations[['STAID','STANAME                                 ']]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
-    temperature = df.loc[df["    DATE"] == date]['   TG'].squeeze() / 10
+    temperature = df.loc[df['    DATE'] == str(date)]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
